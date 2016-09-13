79dfbdc93886163688c96739e67b6a950e0f2904
zhaokai <zhaokai@yidian-inc.com>
2016-09-13 09:42:08 +0800

    Sort each data

--- src/transform.cc
+++ src/transform.cc
@@ -11,11 +11,13 @@
 *      http://www.apache.org/licenses/LICENSE-2.0
 */
 
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <vector>
 
-
+std::string model = "";
 std::string class_name = "";
 std::string case_path = "";
 std::string keywords_path = "";
@@ -24,7 +26,7 @@ int cur_num = 0;
 std::map<std::string, int> keywords;
 
 void Usage() {
-  std::cout << "train class_name case_path keywords_path output_path" << std::endl;
+  std::cout << "transform [train|test] class_name case_path keywords_path output_path" << std::endl;
 }
 
 int InitKeywords() {
@@ -48,7 +50,7 @@ int InitKeywords() {
   return 0;
 }
 
-int ParseCase(std::string* result) {
+int ParseCase(std::vector<std::string>* result) {
   std::fstream kw_out (keywords_path.c_str(),
                        std::fstream::out | std::fstream::app);
   if (!kw_out.is_open()) {
@@ -69,6 +71,7 @@ int ParseCase(std::string* result) {
   while (getline(fs_case, line)) {
     std::cout << "line=" << line << std::endl;
     size_t begin_index = 0;
+    std::vector<int> numbers;
     for (size_t i = 0; i < line.size(); i++) {
       if (!isalpha(line[i]) || i + 1 == line.size()) {
         std::string word = line.substr(begin_index, i - begin_index);
@@ -80,16 +83,24 @@ int ParseCase(std::string* result) {
           int id = 0;
           if (keywords.find(word) != keywords.end()) {
             id = keywords[word];
-          } else {
+          } else if (model == "train") {
             keywords[word] = ++cur_num;
             id = keywords[word];
             kw_out << word << "=" << keywords[word] << std::endl;
           }
-          snprintf(id_buffer, sizeof(id_buffer), "%d:1", id);
-          result->append(std::string(id_buffer) + " ");
+          if (id != 0) {
+            numbers.push_back(id);
+          }
         }
       }
     }
+    std::sort(numbers.begin(), numbers.end());
+    std::string numbers_str = "";
+    for (const auto iter : numbers) {
+      snprintf(id_buffer, sizeof(id_buffer), "%d:1", iter);
+      numbers_str += " " + std::string(id_buffer);
+    }
+    result->push_back(numbers_str);
   }
 
   fs_case.close();
@@ -98,22 +109,24 @@ int ParseCase(std::string* result) {
 }
 
 int main(int argc, char** argv) {
-  if (argc != 5) {
+  if (argc != 6) {
     Usage();
     return -1;
   }
 
-  class_name = argv[1];
-  case_path = argv[2];
-  keywords_path = argv[3];
-  output_path = argv[4];
+  model = argv[1];
+  class_name = argv[2];
+  case_path = argv[3];
+  keywords_path = argv[4];
+  output_path = argv[5];
 
   if (InitKeywords() != 0) {
     return -1;
   }
 
-  std::string output_line = "";
-  if (ParseCase(&output_line) != 0) {
+  std::string output = "";
+  std::vector<std::string> outputs;
+  if (ParseCase(&outputs) != 0) {
     return -1;
   }
 
@@ -123,7 +136,10 @@ int main(int argc, char** argv) {
     std::cout << "Failed to find output_path=" << output_path << "||" << std::endl;
     return -1;
   }
-  out_fs << class_name << " " << output_line << std::endl;
+
+  for (const auto &iter : outputs) {
+    out_fs << class_name << iter << std::endl;
+  }
   out_fs.close();
 
   return 0;
